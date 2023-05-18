c108cd649be865741f6fb00735a052863466798a
Boris Maidana <borismaidana3@gmail.com>
2023-05-18 15:20:12 -0400

    Session singleton factorized

--- SeleniumTraining/src/code/session/Session.cs
+++ SeleniumTraining/src/code/session/Session.cs
@@ -1,33 +1,44 @@
 ﻿using OpenQA.Selenium;
 using SeleniumTraining.src.code.factoryBrowser;
+using SeleniumTraining.src.code.util;
 
 namespace SeleniumTraining.src.code.session
 {
     public class Session
     {
-        private static Session instance = null;
+        private static readonly Lazy<Session> lazyInstance = new Lazy<Session>(() => new Session());
         private IWebDriver browser;
 
         private Session()
         {
-            browser = FactoryBrowser.Make("grid").Create();
+            browser = FactoryBrowser.Make(GetPropertiesYopMail.Instance.Browser).Create();
         }
 
-        public static Session Instance()
-        {
-            if (instance == null) { instance = new Session(); }
-            return instance;
-        }
+        public static Session Instance => lazyInstance.Value;
 
         public void CloseBrowser()
         {
             browser.Quit();
-            instance = null;
         }
 
         public IWebDriver GetBrowser()
         {
             return browser;
+        }
+
+        public void SwitchIFrameMail()
+        {
+            browser.SwitchTo().Frame("ifmail");
+        }
+
+        public void SwitchIFrameInbox()
+        {
+            browser.SwitchTo().Frame("ifinbox");
+        }
+
+        public void SwitchToParent()
+        {
+            browser.SwitchTo().DefaultContent();
         }
     }
 }
