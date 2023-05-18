7e900a7329a279356a9c5aca5f20ca464e78ded9
Boris Maidana <borismaidana3@gmail.com>
2023-05-18 15:22:19 -0400

    Get properties to access to YopmailApp.json

--- SeleniumTraining/src/code/util/GetPropertiesYopMail.cs
+++ SeleniumTraining/src/code/util/GetPropertiesYopMail.cs
@@ -1,20 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace SeleniumTraining.src.code.util
 {
     public class GetPropertiesYopMail
     {
-        private static GetPropertiesYopMail getPropertiesYopMail = null;
-        private string host;
-        private string user;
+        private static readonly Lazy<GetPropertiesYopMail> lazyInstance = new Lazy<GetPropertiesYopMail>(() => new GetPropertiesYopMail());
+        private readonly IConfiguration config;
 
         private GetPropertiesYopMail()
         {
-            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName
+                            + "/src/resources/properties/YopmailApp.json";
+            try
+            {
+                config = new ConfigurationBuilder()
+                            .AddJsonFile(path)
+                            .Build();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading configuration: {ex.Message}");
+            }
         }
+
+        public static GetPropertiesYopMail Instance => lazyInstance.Value;
+
+        public string Browser => config.GetValue<string>("AppSettings:browser");
+        public string Host => config.GetValue<string>("AppSettings:host");
+        public string User => config.GetValue<string>("AppSettings:user");
     }
 }
